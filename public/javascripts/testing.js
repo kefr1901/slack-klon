507fc81ff5e30612edbb6450caa8df93403c4dc5
kefri1901 <kevin.fridman@yh.nackademin.se>
2019-12-09 10:43:22 +0100

    made html and css for the site

---
+++ public/javascripts/testing.js
@@ -0,0 +1,10 @@
+/*document.getElementById("send_btn").addEventListener("click", function(){
+let chat = document.getElementById("textinput").value;
+
+var newchat = document.createElement('p');
+newchat.innerHTML = chat;
+
+var table = document.getElementById('chatbox');
+document.body.appendChild(newchat);
+});*/
+
