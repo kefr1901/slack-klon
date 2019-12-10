3c81d4dcac061f79015915be094080f5fe7f3367
Patrik Johansson <patrik.johansson@yh.nackademin.se>
2019-12-10 16:25:26 +0100

    added add new user

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var monk = require('monk');
+var db = monk('localhost:27017/test');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/users');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,16 +21,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router
+app.use(function (req, res, next) {
+  req.db = db;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,7 +45,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(5000);
 
 module.exports = app;
