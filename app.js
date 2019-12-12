abe429662dfa8875e5e201d6b8f25be04b9dbf57
Ironman <robinneuman@hotmail.com>
2019-12-12 21:01:13 +0100

    added support for rooms

--- app.js
+++ app.js
@@ -7,29 +7,37 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var io = require('socket.io')(server);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-// Object with the names of users
-const users = {};
+const rooms = {};
 
 // Below happens when a user connects
 io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name;
-    socket.broadcast.emit('user-connected', name);
+  socket.on('new-user', (room, name) => {
+    socket.join(room);
+    rooms[room].users[socket.id] = name;
+    socket.to(room).broadcast.emit('user-connected', name);
   })
   // When a message is sent from a user
-  socket.on('send-chat-message', message => {
-      socket.broadcast.emit('chat-message', { message: message, name: 
-      users[socket.id] })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', {
+      message: message, name:
+        rooms[room].users[socket.id]
+    })
   })
   // When a user disconnects from chat
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    });
+  })
 })
-})
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name);
+    return names;
+  }, [])
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,41 +49,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var rooms = {};
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/');
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  io.emit('room-created', req.body.room);
+})
 
 app.get('/', (req, res) => {
-  res.render('index', {rooms: rooms})
+  res.render('index', { rooms: rooms })
 })
 
 // Gets the rooms created (need to make this work with database) and their names
-app.get('/:room', (res, req) => {
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/');
+  }
   res.render('room', { roomName: req.params.room })
 })
 
 // NEED TO ADD ROOM POST
 
 server.listen(3000);
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(5000);
 
 module.exports = app;
--- public/script.js
+++ public/script.js
@@ -2,11 +2,33 @@ const socket = io('http://localhost:3000');
 const msgContainer = document.getElementById('msg-cont');
 const msgForm = document.getElementById('snd-cont');
 const messageInput = document.getElementById('msg-inp');
+const roomCont = document.getElementById('room-cont');
 
-// Prompts user for name (will be replaced by login screen)
-const name = prompt('What is your name?');
-appendMessage('You joined');
-socket.emit('new-user', name);
+if (msgForm != null) {
+    // Prompts user for name (will be replaced by login screen)
+    const name = prompt('What is your name?');
+    appendMessage('You joined');
+    socket.emit('new-user', roomName, name);
+
+    // Listens to submit button and handles data from form
+    msgForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const message = messageInput.value;
+        appendMessage(`You: ${message}`);
+        socket.emit('send-chat-message', roomName, message);
+        messageInput.value = '';
+    })
+}
+
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div');
+    roomElement.innderText = room;
+    const roomLink = document.createElement('a');
+    roomLink.href = `/${room}`;
+    roomLink.innerText = 'join';
+    roomCont.append(roomElement);
+    roomCont.append(roomLink);
+})
 
 // Adds message written onto html page in the specified container
 socket.on('chat-message', data => {
@@ -14,7 +36,7 @@ socket.on('chat-message', data => {
 })
 
 // On user connect, adds user's name and connected message
-socket.on('user-connected', data => {
+socket.on('user-connected', name => {
     appendMessage(`${name} connected`);
 })
 
@@ -23,14 +45,7 @@ socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`);
 })
 
-// Listens to submit button and handles data from form
-msgForm.addEventListener('submit', event => {
-    event.preventDefault();
-    const message = messageInput.value;
-    appendMessage(`You: ${message}`);
-    socket.emit('send-chat-message', message);
-    messageInput.value = '';
-})
+
 
 // Function for adding new div and append received data into them
 function appendMessage(message) {
