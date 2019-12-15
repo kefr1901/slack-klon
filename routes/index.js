63147d6a7e97e66acc7945af74b6b21e3df65bec
kefri1901 <kevin.fridman@yh.nackademin.se>
2019-12-15 13:09:15 +0100

    fixed DB names on site

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,25 @@ router.get('/', function (req, res) {
 });
 
 router.get('/chat', function (req, res) {
-  res.render('chat', { title: 'Add New User' });
+  let db = req.db;
+  let collection = db.get("usercollection");
+  collection.find({}, {}, function (e, data) {
+    console.log(data)
+    if (e) {
+      throw e;
+    }
+    res.render("chat", { data: data })
+
+
+  });
+
+
 });
 
+
+
 /* GET users listing. THIS IS NOT IN USE ATM*/
-router.get('/list', function(req, res, next) {
+router.get('/list', function (req, res, next) {
   var db = req.db;
   var collection = db.get('usercollection');
   collection.find({}, {}, function (e, data) {
@@ -22,17 +36,17 @@ router.get('/list', function(req, res, next) {
 });
 
 /* GET login - Check if there is a user with that name and password, then redirect to get request on line 37 */
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
   let enteredUsername = req.body.username;
   let enteredPassword = req.body.userpassword;
   var db = req.db;
   var collection = db.get('usercollection');
-  collection.findOne({username: enteredUsername, userpassword: enteredPassword}, function (e, data) {
+  collection.findOne({ username: enteredUsername, userpassword: enteredPassword }, function (e, data) {
     console.log(data);
-    if(e || data == null){
+    if (e || data == null) {
       console.log('Username or password is wrong')
       res.redirect('/');
-    }else{
+    } else {
       res.redirect('/chat'); //to avoid having /login in the address field in the browser
     }
   });
@@ -66,7 +80,7 @@ router.post('/chat', function (req, res) {
     }
     else {
       // And forward to success page
-      res.render("chat"); 
+      res.render("chat");
       // res.redirect("list"); this was the old shit when printing all the users as a list
     }
   });
