bd766a2a2ff9cd0b97233187b30dc57296bcb4f2
Patrik Johansson <patrik.johansson@yh.nackademin.se>
2019-12-18 14:37:21 +0100

    fixed image problem

--- routes/upload.js
+++ routes/upload.js
@@ -8,17 +8,17 @@ const path = require('path');
 //set storage engine with multer
 const storage = multer.diskStorage({
     destination: './public/uploads/',
-    filename: function(req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() +//here we say that the name of the file should be the fieldname(myImage)add timestamp and the the path on the file name
-        path.extname(file.originalname)) //add format name on the file name
+            path.extname(file.originalname)) //add format name on the file name
     }
 });
 
 //init upload
 const upload = multer({
     storage: storage, //for storage we are using our storage engine above
-    limits: {fileSize: 1000000}, //sets the limit on the file upload to 1mb
-    fileFilter: function(req, file, cb) {
+    limits: { fileSize: 1000000 }, //sets the limit on the file upload to 1mb
+    fileFilter: function (req, file, cb) {
         //check filetype
         function checkFile(file, cb) {
             //which extensions that are allowed
@@ -27,7 +27,7 @@ const upload = multer({
             const extname = fileTypes.test(path.extname(file.originalname).toLocaleLowerCase());
             //check mimetype
             const mimetype = fileTypes.test(file.mimetype);
-            if(mimetype && extname) {
+            if (mimetype && extname) {
                 return cb(null, true);
             } else {
                 cb('Error: Images only');
@@ -38,8 +38,10 @@ const upload = multer({
 
 //when submitting an image
 router.post('/', (req, res) => {
+    console.log(req.files);
+
     upload(req, res, (err) => {
-        if(err) {
+        if (err) {
             res.render('chat', { //if error then rerender the page and send in the error into the variable msg that consist on the .ejs-file
                 msg: err
             });
@@ -49,12 +51,28 @@ router.post('/', (req, res) => {
                     msg: 'Error: No file selected'
                 });
             } else {
-                res.sendFile(`uploads/${req.file.filename}`);
-                // res.render('chat', { //här har jag ändrat lite lallal kul!
-                //     // msg: 'File uploaded',
-                //     file: `uploads/${req.file.filename}`
-                // });
-                res.render('chat');
+                let db = req.db;
+                let collection = db.get("usercollection");
+                collection.find({}, {}, function (e, data) {
+                    console.log('daataaa:' + data)
+                    if (e) {
+                        throw e;
+                    } else {
+                        //HÄR HADE MAN KUNNAT ATT KANSKE SATT EN SÖKVÄG TILL BILDEN TILL ANVÄNDAREN
+                        res.cookie('user', req.session.user._id, { maxAge: 3600, httpOnly: false });
+                        console.log('uid: ' + req.session.user._id);
+                        //res.render("chat", { data: data })
+
+                        // res.sendFile(`uploads/${req.file.filename}`);
+                        res.render('chat', { //här har jag ändrat lite lallal kul!
+                        // msg: 'File uploaded',
+                            file: `uploads/${req.file.filename}`,
+                            data: data
+                        });
+                    };
+                    
+                    // res.render('chat');
+                });
             }
         }
     });
